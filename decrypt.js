dcd771ea27b498aac568bbbaa38989e23b117a3a
Giovanni <giovanni@learntheropes.xyz>
2025-03-20 10:46:12 -0300

    first commit

---
+++ decrypt.js
@@ -0,0 +1,21 @@
+const CryptoJS = require('react-native-crypto-js');
+
+const decrypt = (encryptedText, password) => {
+  const bytes = CryptoJS.AES.decrypt(encryptedText, password);
+  return bytes.toString(CryptoJS.enc.Utf8);
+};
+
+const str = process.argv[2];
+const password = process.argv[3];
+
+if (!str || !password) {
+  console.error('Usage: node decrypt.js <encryptedFilePath> <password>');
+  process.exit(1);
+}
+
+const decyptedText = decrypt(str, password);
+const content = JSON.parse(decyptedText);
+
+console.log(`\nMnemonic:\n${content.mnemonic}\n`);
+console.log(`PGP Private Key:\n${content.pgp.privateKey}\n`);
+console.log(`PGP Public Key:\n${content.pgp.publicKey}`);
